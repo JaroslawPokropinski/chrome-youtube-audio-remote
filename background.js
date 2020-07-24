163c1ccdaddeb7621ad2af5ba9354c2e6717a61e
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-07-24 17:24:05 +0200

    Fix shortcuts not working

--- background.js
+++ background.js
@@ -11,37 +11,37 @@ chrome.runtime.onInstalled.addListener(function() {
           actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
+});
 
-  const dv = 0.01;
-  chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-    if (command === 'volume_up' || command === 'volume_up2') {
-      const cdv = (command === 'volume_up')? dv : dv * 2;
-      chrome.tabs.query({url: 'https://www.youtube.com/*'}, function(tabs) {
-        tabs.forEach(tab => {
-          chrome.tabs.executeScript(
-            tab.id,
-            { code: `document.querySelector('.video-stream').volume += ${cdv};` });
-        });
+const dv = 0.01;
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('Command:', command);
+  if (command === 'volume_up' || command === 'volume_up2x') {
+    const cdv = (command === 'volume_up')? dv : dv * 2;
+    chrome.tabs.query({url: 'https://www.youtube.com/*'}, function(tabs) {
+      tabs.forEach(tab => {
+        chrome.tabs.executeScript(
+          tab.id,
+          { code: `document.querySelector('.video-stream').volume += ${cdv};` });
       });
-        
-      return;
-    }
+    });
+      
+    return;
+  }
 
-    if (command === 'volume_down' || command === 'volume_down2') {
-      const cdv = (command === 'volume_down')? dv : dv * 2;
-      chrome.tabs.query({url: 'https://www.youtube.com/*'}, function(tabs) {
-        tabs.forEach(tab => {
-          chrome.tabs.executeScript(
-            tab.id,
-            { code: `if (document.querySelector('.video-stream').volume > ${cdv}) document.querySelector('.video-stream').volume -= ${cdv}; else document.querySelector('.video-stream').volume = 0;` });
-        });
+  if (command === 'volume_down' || command === 'volume_down2x') {
+    const cdv = (command === 'volume_down')? dv : dv * 2;
+    chrome.tabs.query({url: 'https://www.youtube.com/*'}, function(tabs) {
+      tabs.forEach(tab => {
+        chrome.tabs.executeScript(
+          tab.id,
+          { code: `if (document.querySelector('.video-stream').volume > ${cdv}) document.querySelector('.video-stream').volume -= ${cdv}; else document.querySelector('.video-stream').volume = 0;` });
       });
-        
-      return;
-    }
+    });
+      
+    return;
+  }
 
-    console.log('Unknown command:', command)
+  console.log('Unknown command:', command)
 
-  });
 });
--- popup.js
+++ popup.js
@@ -18,26 +18,3 @@ volumeInput.onchange = function(element) {
         { code: `document.querySelector('.video-stream').volume = ${parseFloat(volumeInput.value) / 1000};` });
   });
 }
-
-// chrome.commands.onCommand.addListener(function(command) {
-//   if (command === 'volume up') {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.executeScript(
-//           tabs[0].id,
-//           { code: `document.querySelector('.video-stream').volume += ${0.05};` });
-//     });
-//     return;
-//   }
-
-//   if (command === 'volume down') {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.executeScript(
-//           tabs[0].id,
-//           { code: `document.querySelector('.video-stream').volume -= ${0.05};` });
-//     });
-//     return;
-//   }
-
-//   console.log('Unknown command:', command)
-
-// });
